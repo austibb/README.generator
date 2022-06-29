d312433eb56dcc7d6c82512533986eff9c5b265b
Austin Wu <austinwu@Austins-Mac-mini.local>
2022-06-28 20:04:44 -0400

    updating files

---
+++ Develop/README.md
@@ -0,0 +1,53 @@
+# sldkfjadlsk
+  ![GitHub license](https://img.shields.io/badge/license-GPL&3.0-blue.svg)
+
+  ## Description
+  
+  lskdjfljsd
+  
+  ## Table of Contents 
+  
+  * [Installation](#installation)
+  
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+  
+  * [Tests](#tests)
+  
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  To install necessary dependencies, run the following command:
+  
+  ```
+  ljsdlkfs
+  ```
+  
+  ## Usage
+  
+  lsdkfjlsd
+
+  ## Licensing
+  
+  This project is licensed under the GPL 3.0
+    
+  ## Contributing
+  
+  sllsdkjf
+  
+  ## Tests
+  
+  To run tests, run the following command:
+  
+  ```
+  lsdkfjlsdjf
+  ```
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly at lsdkfjsdlk. You can find more of my work at [lskdjfa](https://github.com/lskdjfa/).
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,74 @@
-// TODO: Include packages needed for this application
+// packages and files needed for program to function
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateReadMe = require('./utils/generateReadMe');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// array of the prompts the user will see when running this program
+const prompts = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your Github username: ',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address:',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter your project name:',
+        default: 'My Project',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description for your project:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select the appropriate license for your project:',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+        default: "None",
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What commands should the user run to install dependencies?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What commands should the user run to test the project?',
+        default: 'npm test',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please list considerations for the user to account for when using this app:',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Can users contribute to the repo? If so, how?',
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// creates a new file with the given file name and file content
+let writeToFile = (fileName, data) => fs.writeFileSync(path.join(process.cwd(), fileName), data);
 
-// TODO: Create a function to initialize app
-function init() {}
+// initialization function
+function init() {
+    inquirer
+        .prompt(prompts)
+        .then((response) => {
+            console.log('Generating README...');
+            writeToFile('README.md', generateReadMe({ ...response }));
+            console.log('File created! Good luck with your project.');
+        });
+}
 
-// Function call to initialize app
 init();
